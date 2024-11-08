5beb85823e7b3264610c6b4f387d95b61ccf8779
user <agagylrustamov@gmail.com>
2024-11-08 13:29:12 +0400

    init

---
+++ src/main/java/rustamscode/learningspringrest/LearningSpringRestApplication.java
@@ -0,0 +1,13 @@
+package rustamscode.learningspringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningSpringRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rustamscode/learningspringrest/LearningSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package rustamscode.learningspringrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
